1b0626bca89f2434071af319bddbcedf0f9efdef
Roman <roman-meinl@ukr.net>
2025-02-28 14:48:41 -0200

    second less

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,18 @@
+
+import {MyName} from "../MyName/MyName.jsx";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+        <p>This my first Project</p>
+        <MyName />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MyName/MyName.jsx
@@ -0,0 +1,8 @@
+import "./MyName.css";
+
+
+export  function MyName(){
+    return <h2>My Name is Roman</h2>
+        
+    
+}
